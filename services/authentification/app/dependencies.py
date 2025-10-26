0e9aac65b8e03deace7cfe42dd3735492072f5e6
Be3yH4uK315 <dkosterin3@gmail.com>
2025-10-26 22:47:25 +0700

    chore: remove unused rate_limiter function

--- services/authentification/app/dependencies.py
+++ services/authentification/app/dependencies.py
@@ -1,12 +1,12 @@
-from fastapi import Depends
+from contextlib import asynccontextmanager
 from redis.asyncio import Redis
-from fastapi_limiter import FastAPILimiter
 from .settings import settings
-from .db import get_db
 
-async def get_redis() -> Redis:
+
+@asynccontextmanager
+async def get_redis():
     redis = Redis.from_url(settings.redis_url, decode_responses=True)
-    yield redis
-
-async def rate_limiter(request, call_next):
-    pass
+    try:
+        yield redis
+    finally:
+        await redis.close()
