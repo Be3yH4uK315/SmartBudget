02822a057469a67f2f7dba1e61c4f179efdf8b70
anton <sedleczkij.2022@stud.nstu.ru>
2025-10-27 21:20:49 +0700

    chore: normalize App component path to src/app/App.tsx

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,12 @@
+import { RootLayout } from './RootLayout'
+
+export function App() {
+  return (
+    <RootLayout>
+      <div>
+        <h1>Rsbuild with React</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+      </div>
+    </RootLayout>
+  )
+}
