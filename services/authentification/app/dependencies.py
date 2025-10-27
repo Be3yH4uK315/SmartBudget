956d7a9f6e2698562fc54d6d2668c3f5a43c3a20
Be3yH4uK315 <dkosterin3@gmail.com>
2025-10-27 21:49:45 +0700

    refactor: implement connection pooling via lifespan

--- services/authentification/app/dependencies.py
+++ services/authentification/app/dependencies.py
@@ -1,8 +1,9 @@
-from fastapi import Depends
-from redis.asyncio import Redis
+from fastapi import Depends, Request
+from redis.asyncio import Redis, ConnectionPool
 from app.settings import settings
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from typing import AsyncGenerator
 
 # --- DB ---
 engine = create_async_engine(settings.db_url)
@@ -11,10 +12,17 @@ async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit
 class Base(DeclarativeBase):
     pass
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
 
 # --- Redis ---
-async def get_redis() -> Redis:
-    return Redis.from_url(settings.redis_url, decode_responses=True)
+async def get_redis(request: Request) -> Redis:
+    pool: ConnectionPool = request.app.state.redis_pool
+    return Redis(connection_pool=pool, decode_responses=True)
+
+async def create_redis_pool() -> ConnectionPool:
+    return ConnectionPool.from_url(settings.redis_url)
+
+async def close_redis_pool(pool: ConnectionPool):
+    await pool.disconnect()
--- services/authentification/app/main.py
+++ services/authentification/app/main.py
@@ -1,22 +1,24 @@
+import Redis
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 from contextlib import asynccontextmanager
 from app.middleware import error_middleware, setup_logging
 from app.routers.auth import router as auth_router
 from app.kafka import startup_kafka, shutdown_kafka
-from app.dependencies import get_redis
+from app.dependencies import create_redis_pool, close_redis_pool, get_redis
 from fastapi_limiter import FastAPILimiter
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     setup_logging()
     await startup_kafka()
-    # НАДО исправить
-    redis_client = get_redis()
+    redis_pool = await create_redis_pool()
+    app.state.redis_pool = redis_pool
+    redis_client = Redis(connection_pool=redis_pool, decode_responses=True)
     await FastAPILimiter.init(redis_client)
     yield
     await shutdown_kafka()
-    await redis_client.close()
+    await close_redis_pool(redis_pool)
     await FastAPILimiter.close()
 
 app = FastAPI(title="Auth Service", version="1.0", lifespan=lifespan)
