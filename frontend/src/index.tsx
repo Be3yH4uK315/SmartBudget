1fca644f8f5876aeb6e8f4504511d48fddba67f5
anton <sedleczkij.2022@stud.nstu.ru>
2025-10-27 20:51:39 +0700

    chore: update env.d.ts & index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client'
+import { App } from '@app'
 
-const rootEl = document.getElementById('root');
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+  root.render(<App />)
 }
