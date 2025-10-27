8bedb7104b3f7fa8823952f9935414bc735bb571
anton <sedleczkij.2022@stud.nstu.ru>
2025-10-27 23:16:41 +0700

    feat: connect Providers with App

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,12 @@
+import { Providers } from './providers'
 import { RootLayout } from './RootLayout'
 
 export function App() {
   return (
-    <RootLayout>
-      <div>
-        <h1>Rsbuild with React</h1>
-        <p>Start building amazing things with Rsbuild.</p>
-      </div>
-    </RootLayout>
+    <Providers>
+      <RootLayout>
+        <></>
+      </RootLayout>
+    </Providers>
   )
 }
