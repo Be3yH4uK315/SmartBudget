78f030d3f6d6181a28d6b0daea8118c0ade1e932
anton <sedleczkij.2022@stud.nstu.ru>
2025-10-27 21:34:48 +0700

    feat add useTheme hook

---
+++ frontend/src/shared/hooks/useTheme.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import { ThemeContext } from '@app/providers/ThemeProvider'
+
+export function useTheme() {
+  const context = useContext(ThemeContext)
+
+  if (!context) throw new Error('ThemeContext context not found')
+
+  return context
+}
