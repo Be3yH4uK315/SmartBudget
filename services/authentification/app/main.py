d709b6898f12219c0a36101023002fc19639a0b6
Be3yH4uK315 <dkosterin3@gmail.com>
2025-10-27 21:39:57 +0700

    chore: Redis issues to be fixed in the future

--- services/authentification/app/main.py
+++ services/authentification/app/main.py
@@ -7,23 +7,19 @@ from app.kafka import startup_kafka, shutdown_kafka
 from app.dependencies import get_redis
 from fastapi_limiter import FastAPILimiter
 
-app = FastAPI(title="Auth Service", version="1.0")
-Instrumentator().instrument(app).expose(app)
-app.middleware("http")(error_middleware)
-app.include_router(auth_router)
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     setup_logging()
     await startup_kafka()
-    async with get_redis() as redis:
-        await FastAPILimiter.init(redis)
-        yield
+    # НАДО исправить
+    redis_client = get_redis()
+    await FastAPILimiter.init(redis_client)
+    yield
     await shutdown_kafka()
+    await redis_client.close()
     await FastAPILimiter.close()
 
-app.router.lifespan_context = lifespan
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, workers=4, reload=True)
+app = FastAPI(title="Auth Service", version="1.0", lifespan=lifespan)
+Instrumentator().instrument(app).expose(app)
+app.middleware("http")(error_middleware)
+app.include_router(auth_router)
