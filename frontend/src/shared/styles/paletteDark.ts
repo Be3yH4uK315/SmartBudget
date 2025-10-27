a4f74a23ded7f1cce6f1d85aa0e116430c19aa40
anton <sedleczkij.2022@stud.nstu.ru>
2025-10-27 21:29:05 +0700

    feat: add theme types & light/dark palette

---
+++ frontend/src/shared/styles/paletteDark.ts
@@ -0,0 +1,96 @@
+import { PaletteOptions } from '@mui/material/styles'
+
+export const paletteDark: PaletteOptions = {
+  primary: {
+    light: '#FFCD33', //buttonHover
+    main: '#FFDD2D',
+    dark: '#FAB619', //buttonPressed
+  },
+
+  secondary: {
+    light: '#126DF7', //hover
+    main: '#428BF9',
+    dark: '#156ED4', //buttonPressed
+  },
+
+  surface: {
+    light: '#5F5F5F',
+    main: '#191919',
+    dark: '#000000',
+  },
+
+  gray: {
+    light: '#E2E2E4',
+    main: '#747B8F',
+    dark: '#333333',
+  },
+
+  link: {
+    light: '#8B4EA6', //visited
+    main: '#428DF9',
+    dark: '#165ECA', //hover
+  },
+
+  success: {
+    light: '#B9F8CF',
+    main: '#00A63E',
+    dark: '#016630'
+  },
+
+  info: {
+    light: '#BEDBFF',
+    main: '#155DFC',
+    dark: '#193CB8'
+  },
+
+  alert: {
+      light: '#FFF085',
+      main: '#FFAB00',
+      dark: '#894B00',
+  },
+
+  grayButton: {
+    main: '#F3F4F7',
+    dark: '#EBEFF3', //hover
+  },
+
+  error: {
+    light: '#FFC9C9',
+    main: '#E7000B',
+    dark: '#9F0712'
+  },
+
+  text: {
+    primary: '#FFFFFF',
+  },
+
+  additionalRed: {
+    main: '#FE788B',
+    light: '#FE788BBF', 
+  },
+
+  additionalLavender: {
+    main: '#BAA2ED',
+    light: '#BAA2EDBF', 
+  },
+
+  additionalOrange: {
+    main: '#F8B975',
+    light: '#F8B975BF', 
+  },
+
+  additionalPurple: {
+    main: '#6966F8',
+    light: '#6966F8BF',
+  },
+
+  additionalBlue: {
+    main: '#4FC5DF',
+    light: '#4FC5DFBF', 
+  },
+
+  additionalGray: {
+    main: '#B5CED9',
+    light: '#B5CED9BF',
+  },
+}
---
+++ frontend/src/shared/types/theme/theme.d.ts
@@ -0,0 +1,46 @@
+import { PaletteOptions as MuiPaletteOptions } from '@mui/material/styles'
+import '@mui/material/Button'
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsVariantOverrides {
+    gray: true
+    yellow: true
+    blue: true
+  }
+}
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    gray: Palette['primary']
+    surface: Palette['primary']
+    link: Palette['primary']
+    info: Palette['primary']
+    text: Palette['primary']
+    alert: Palette['primary']
+    grayButton: Palette['primary']
+    additionalRed: Palette['primary']
+    additionalPurple: Palette['primary']
+    additionalBlue: Palette['primary']
+    additionalLavender: Palette['primary']
+    additionalOrange: Palette['primary']
+    additionalGray: Palette['primary']
+  }
+
+  interface PaletteOptions {
+    gray: MuiPaletteOptions['primary']
+    surface: MuiPaletteOptions['primary']
+    link: MuiPaletteOptions['primary']
+    info: MuiPaletteOptions['primary']
+    text: MuiPaletteOptions['primary']
+    alert: MuiPaletteOptions['primary']
+    grayButton: MuiPaletteOptions['primary']
+    additionalRed: MuiPaletteOptions['primary']
+    additionalPurple: MuiPaletteOptions['primary']
+    additionalBlue: MuiPaletteOptions['primary']
+    additionalLavender: MuiPaletteOptions['primary']
+    additionalOrange: MuiPaletteOptions['primary']
+    additionalGray: MuiPaletteOptions['primary']
+  }
+
+  type SvgIconPropsColorOverrides = 'gray'
+}
