d0c21d8cca998fe9fc9175affd7ef0aed75adaf5
anton <sedleczkij.2022@stud.nstu.ru>
2025-10-28 17:29:38 +0700

    feat: add LocalizationProvider into Providers.tsx

--- frontend/src/app/providers/Providers.tsx
+++ frontend/src/app/providers/Providers.tsx
@@ -2,13 +2,14 @@ import { PropsWithChildren } from 'react'
 import { ReduxProvider } from './ReduxProvider'
 import { ThemeProvider } from './ThemeProvider'
 import { RouterProvider } from './RouterProvider'
+import { LocalizationProvider } from './LocalizationProvider'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <ReduxProvider>
       <RouterProvider>
         <ThemeProvider>
-          {children}
+          <LocalizationProvider>{children}</LocalizationProvider>
         </ThemeProvider>
       </RouterProvider>
     </ReduxProvider>
