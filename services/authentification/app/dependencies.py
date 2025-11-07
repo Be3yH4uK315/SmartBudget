5d504776502523e8a37a42813c3e53b7032c1e23
Be3yH4uK315 <dkosterin3@gmail.com>
2025-11-07 19:34:28 +0700

    feat: add base.py for models and dependencies, fix func get_current_user_id, upgraded redis pool and arq pool

--- services/authentification/app/dependencies.py
+++ services/authentification/app/dependencies.py
@@ -1,29 +1,32 @@
 from fastapi import Depends, Request, HTTPException
 from redis.asyncio import Redis, ConnectionPool
-from app.settings import settings
+from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
 from typing import AsyncGenerator
 from arq.connections import ArqRedis
-from jwt import decode, PyJWTError
+from jwt import ExpiredSignatureError, InvalidSignatureError, decode, PyJWTError
 import geoip2.database
-from app.middleware import logger
+
+from .settings import settings
+from .middleware import logger
+from .models import User
 
 # --- DB ---
 engine = create_async_engine(settings.db_url)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-class Base(DeclarativeBase):
-    pass
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
 
 # --- Redis ---
-async def get_redis(request: Request) -> Redis:
+async def get_redis(request: Request) -> AsyncGenerator[Redis, None]:
     pool: ConnectionPool = request.app.state.redis_pool
-    return Redis(connection_pool=pool, decode_responses=True)
+    redis = Redis(connection_pool=pool, decode_responses=True)
+    try:
+        yield redis
+    finally:
+        await redis.aclose()
 
 async def create_redis_pool() -> ConnectionPool:
     return ConnectionPool.from_url(settings.redis_url, decode_responses=True)
@@ -32,8 +35,12 @@ async def close_redis_pool(pool: ConnectionPool):
     await pool.disconnect()
 
 # --- Arq ---
-async def get_arq_pool(request: Request) -> ArqRedis:
-    return request.app.state.arq_pool
+async def get_arq_pool(request: Request) -> AsyncGenerator[ArqRedis, None]:
+    arq_pool: ArqRedis = request.app.state.arq_pool
+    try:
+        yield arq_pool
+    finally:
+        pass
 
 def get_geoip_reader(request: Request) -> geoip2.database.Reader:
     """
@@ -50,7 +57,10 @@ def get_real_ip(request: Request) -> str:
         return request.headers["x-forwarded-for"].split(",")[0].strip()
     return request.client.host if request.client else "127.0.0.1"
 
-async def get_current_user_id(request: Request) -> str:
+async def get_current_user_id(
+    request: Request,
+    db: AsyncSession = Depends(get_db)
+) -> str:
     access_token = request.cookies.get("access_token")
     if not access_token:
         raise HTTPException(
@@ -61,11 +71,25 @@ async def get_current_user_id(request: Request) -> str:
         payload = decode(
             access_token,
             settings.jwt_public_key,
-            algorithms=[settings.jwt_algorithm]
+            algorithms=[settings.jwt_algorithm],
+            audience='smart-budget'
         )
         user_id: str | None = payload.get("sub")
         if user_id is None:
             raise HTTPException(status_code=401, detail="Invalid token (missing sub)")
+        
+        try:
+            user = await db.get(User, UUID(user_id))
+            if not user or not user.is_active:
+                raise HTTPException(status_code=401, detail="User inactive or not found")
+        except Exception:
+            raise HTTPException(status_code=401, detail="Invalid token payload")
+
         return user_id
-    except PyJWTError:
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except InvalidSignatureError:
+        raise HTTPException(status_code=401, detail="Invalid token signature (Key mismatch)")
+    except PyJWTError as e:
+        print(f">>> PyJWTError caught: {e}") 
         raise HTTPException(status_code=401, detail="Invalid token")
