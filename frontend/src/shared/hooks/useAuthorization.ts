35bf1efb1dbb667ed5f420e280ec0b5e6ed667ac
anton <sedleczkij.2022@stud.nstu.ru>
2025-11-17 13:49:32 +0700

    feat: add useAuthorization hook

---
+++ frontend/src/shared/hooks/useAuthorization.ts
@@ -0,0 +1,33 @@
+import { authApi } from '@shared/api/auth'
+import { getUserInfo } from '@shared/store/user'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+export function useAuthorization() {
+  const dispatch = useDispatch<AppDispatch>()
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const auth = async () => {
+      try {
+        const refreshResponse = await authApi.refresh()
+
+        if (refreshResponse !== 200) return
+
+        const action = await dispatch(getUserInfo())
+
+        if (getUserInfo.rejected.match(action) && action.payload === 'noInfo') {
+          await authApi.logout()
+        }
+      } catch (_) {
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    auth()
+  }, [dispatch])
+
+  return { isLoading }
+}
