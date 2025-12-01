85367d7141cdf9af6ec51f45b7af8727c27667d2
Be3yH4uK315 <dkosterin3@gmail.com>
2025-12-01 14:39:49 +0700

    refactor: Centralize logging, split settings, and add custom exceptions

--- services/classification/app/settings.py
+++ services/classification/app/settings.py
@@ -3,37 +3,52 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
-
 env_file_name = os.getenv("ENV_FILE", ".env")
 env_file_path = ROOT_DIR / env_file_name
 
-class Settings(BaseSettings):
-    """
-    Настройки сервиса категоризации.
-    """
-    env: str = 'dev'
+class DBSettings(BaseSettings):
     db_url: str
+
+class KafkaSettings(BaseSettings):
     kafka_bootstrap_servers: str
     kafka_group_id: str
-    redis_url: str
     topic_need_category: str
     topic_classified: str
     topic_classification_events: str
     topic_updated: str
     topic_need_category_dlq: str
+
+class RedisSettings(BaseSettings):
+    redis_url: str
+
+class ArqSettings(BaseSettings):
     arq_queue_name: str
+
+class MLSettings(BaseSettings):
     model_path: str
     dataset_path: str
     ml_confidence_threshold_accept: float
     ml_confidence_threshold_audit: float
-    frontend_url: str = "http://localhost:3000"
-    prometheus_port: int = 8001
-    log_level: str = "INFO"
-    tz: str = "UTC"
+
+class AppSettings(BaseSettings):
+    env: str
+    frontend_url: str
+    prometheus_port: int
+    log_level: str
+    tz: str
+
+class Settings(BaseSettings):
+    db: DBSettings
+    kafka: KafkaSettings
+    redis: RedisSettings
+    arq: ArqSettings
+    ml: MLSettings
+    app: AppSettings
 
     model_config = SettingsConfigDict(
         env_file=str(env_file_path),
         env_file_encoding="utf-8",
+        env_nested_delimiter="__"
     )
 
 settings = Settings()
