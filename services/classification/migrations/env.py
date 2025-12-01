84cfdb99652be6edfe2f83ab86c7cfc97f052d31
Be3yH4uK315 <dkosterin3@gmail.com>
2025-12-01 18:41:16 +0700

    infra: Correct docker files and migrations

--- services/classification/migrations/env.py
+++ services/classification/migrations/env.py
@@ -13,7 +13,7 @@ from app.models import Category, Rule, ClassificationResult, Feedback, Model
 
 # ---- Alembic Config ----
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.db_url)
+config.set_main_option('sqlalchemy.url', settings.db.db_url)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 target_metadata = Base.metadata
