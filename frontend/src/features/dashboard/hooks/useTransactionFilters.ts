8d9026163aa1399c8fccd21c14d1b80a3e38bee9
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-05 01:58:38 +0700

    feat: add type for useMemo

--- frontend/src/features/dashboard/hooks/useTransactionFilters.ts
+++ frontend/src/features/dashboard/hooks/useTransactionFilters.ts
@@ -12,51 +12,53 @@ export function useTransactionFilters(data: DashboardCategory[]) {
     setActiveType(type)
   }, [])
 
-  const { normalizedData, total }: { normalizedData: normalizedCategory[]; total: number } =
-    useMemo(() => {
-      const colors = [
-        {
-          main: theme.palette.additionalRed.main,
-          light: theme.palette.additionalRed.light,
-        },
-        {
-          main: theme.palette.additionalBlue.main,
-          light: theme.palette.additionalBlue.light,
-        },
-        {
-          main: theme.palette.additionalLavender.main,
-          light: theme.palette.additionalLavender.light,
-        },
-        {
-          main: theme.palette.additionalOrange.main,
-          light: theme.palette.additionalOrange.light,
-        },
-        {
-          main: theme.palette.additionalGray.main,
-          light: theme.palette.additionalGray.light,
-        },
-        {
-          main: theme.palette.additionalPurple.main,
-          light: theme.palette.additionalPurple.light,
-        },
-      ]
+  const { normalizedData, total } = useMemo<{
+    normalizedData: normalizedCategory[]
+    total: number
+  }>(() => {
+    const colors = [
+      {
+        main: theme.palette.additionalRed.main,
+        light: theme.palette.additionalRed.light,
+      },
+      {
+        main: theme.palette.additionalBlue.main,
+        light: theme.palette.additionalBlue.light,
+      },
+      {
+        main: theme.palette.additionalLavender.main,
+        light: theme.palette.additionalLavender.light,
+      },
+      {
+        main: theme.palette.additionalOrange.main,
+        light: theme.palette.additionalOrange.light,
+      },
+      {
+        main: theme.palette.additionalGray.main,
+        light: theme.palette.additionalGray.light,
+      },
+      {
+        main: theme.palette.additionalPurple.main,
+        light: theme.palette.additionalPurple.light,
+      },
+    ]
 
-      const normalizedData = data
-        .filter((c) => Number(c.value) > 0)
-        .filter((c) => c.type === activeType)
-        .sort((a, b) => b.value - a.value)
-        .map((c, idx) => ({
-          value: Number(c.value),
-          label: translateCategory(`${c.categoryId}`),
-          color: colors[idx % colors.length].main,
-          lightColor: colors[idx % colors.length].light,
-        }))
+    const normalizedData = data
+      .filter((c) => Number(c.value) > 0)
+      .filter((c) => c.type === activeType)
+      .sort((a, b) => b.value - a.value)
+      .map((c, idx) => ({
+        value: Number(c.value),
+        label: translateCategory(`${c.categoryId}`),
+        color: colors[idx % colors.length].main,
+        lightColor: colors[idx % colors.length].light,
+      }))
 
-      return {
-        normalizedData,
-        total: normalizedData.reduce((sum, c) => sum + c.value, 0),
-      }
-    }, [data, activeType, theme, translateCategory])
+    return {
+      normalizedData,
+      total: normalizedData.reduce((sum, c) => sum + c.value, 0),
+    }
+  }, [data, activeType, theme, translateCategory])
 
   return { activeType, toggleFilter, normalizedData, total }
 }
