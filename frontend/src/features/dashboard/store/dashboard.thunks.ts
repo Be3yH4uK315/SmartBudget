4895a1318041c4af2b73f0a3e0f2c4e7d57d90d2
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-07 10:42:53 +0700

    feat: add rejectedWithValue into dashboard thunk

--- frontend/src/features/dashboard/store/dashboard.thunks.ts
+++ frontend/src/features/dashboard/store/dashboard.thunks.ts
@@ -2,6 +2,16 @@ import { dashboardApi } from '@features/dashboard/api'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { dashboardMock } from '../api/dashboard.mock'
 
-export const getDashboardData = createAsyncThunk<DashboardResponsePayload, void>('getDashboardData', () =>
-  dashboardMock.getDashboardData(),
-)
+export const getDashboardData = createAsyncThunk<
+  DashboardResponsePayload,
+  void,
+  { rejectValue: 'cannotGetDashboardData' }
+>('getDashboardData', async (_, { rejectWithValue }) => {
+  try {
+    const response = await dashboardMock.getDashboardData()
+
+    return response
+  } catch (e: any) {
+    return rejectWithValue('cannotGetDashboardData')
+  }
+})
