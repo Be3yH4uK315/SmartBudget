6aef5188932a755c399ee3da2acb5d06902fcbca
4y8a4ek <68740549+4y8a4ek@users.noreply.github.com>
2025-12-08 20:03:01 +0700

    Refactor: Remove JsonProperty attributes from DTOs
    Удалил атрибуты JsonPropertyName из DTO, теперь свойства используют стандартные имена C#

--- services/transactions/SmartBudget.transactions/DTO/CreateManualTransactionRequest.cs
+++ services/transactions/SmartBudget.transactions/DTO/CreateManualTransactionRequest.cs
@@ -8,27 +8,16 @@ namespace SmartBudget.Transactions.Domain.DTO
 /// </summary>
 public class CreateManualTransactionRequest
 {
-[JsonPropertyName("USER_ID")]
 public string UserId { get; set; }
 
-
-[JsonPropertyName("ACCOUNT_ID")]
 public string AccountId { get; set; }
 
-
-[JsonPropertyName("VALUE")]
 public decimal Value { get; set; }
 
-
-[JsonPropertyName("CATEGORY_ID")]
 public int? CategoryId { get; set; }
 
-
-[JsonPropertyName("DESCRIPTION")]
 public string Description { get; set; }
 
-
-[JsonPropertyName("NAME")]
 public string Name { get; set; }
 }
 }
