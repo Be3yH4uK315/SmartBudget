598725e4ad9aee85aa6b7c81e69bac333bd9feaf
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-15 20:47:14 +0700

    ref: remove lazy import for IconComponent

--- frontend/src/features/transactions/constants/categoriesIcons.ts
+++ frontend/src/features/transactions/constants/categoriesIcons.ts
@@ -1,73 +1,70 @@
-import { lazy } from 'react'
+import AirplanemodeActiveOutlined from '@mui/icons-material/AirplanemodeActiveOutlined'
+import AutoAwesomeOutlined from '@mui/icons-material/AutoAwesomeOutlined'
+import AutoStoriesOutlined from '@mui/icons-material/AutoStoriesOutlined'
+import BlenderOutlined from '@mui/icons-material/BlenderOutlined'
+import BuildOutlined from '@mui/icons-material/BuildOutlined'
+import CheckroomOutlined from '@mui/icons-material/CheckroomOutlined'
+import CloudDoneOutlined from '@mui/icons-material/CloudDoneOutlined'
+import ConstructionOutlined from '@mui/icons-material/ConstructionOutlined'
+import DirectionsBusOutlined from '@mui/icons-material/DirectionsBusOutlined'
+import FastfoodOutlined from '@mui/icons-material/FastfoodOutlined'
+import FitnessCenterOutlined from '@mui/icons-material/FitnessCenterOutlined'
+import KingBedOutlined from '@mui/icons-material/KingBedOutlined'
+import LocalActivityOutlined from '@mui/icons-material/LocalActivityOutlined'
+import LocalFlorist from '@mui/icons-material/LocalFlorist'
+import LocalGasStationOutlined from '@mui/icons-material/LocalGasStationOutlined'
+import LocalParkingOutlined from '@mui/icons-material/LocalParkingOutlined'
+import LocalTaxiOutlined from '@mui/icons-material/LocalTaxiOutlined'
+import MedicalInformationOutlined from '@mui/icons-material/MedicalInformationOutlined'
+import PetsOutlined from '@mui/icons-material/PetsOutlined'
+import RealEstateAgentOutlined from '@mui/icons-material/RealEstateAgentOutlined'
+import RouterOutlined from '@mui/icons-material/RouterOutlined'
+import SavingsOutlined from '@mui/icons-material/SavingsOutlined'
+import SchoolOutlined from '@mui/icons-material/SchoolOutlined'
+import SettingsOutlined from '@mui/icons-material/SettingsOutlined'
+import ShoppingBagOutlined from '@mui/icons-material/ShoppingBagOutlined'
+import ShoppingCartOutlined from '@mui/icons-material/ShoppingCartOutlined'
+import SpaOutlined from '@mui/icons-material/SpaOutlined'
+import SportsEsportsOutlined from '@mui/icons-material/SportsEsportsOutlined'
+import StorefrontOutlined from '@mui/icons-material/StorefrontOutlined'
+import VaccinesOutlined from '@mui/icons-material/VaccinesOutlined'
 import { SvgIconTypeMap } from '@mui/material'
 import { OverridableComponent } from '@mui/material/OverridableComponent'
 
-const Supermarkets = lazy(() => import('@mui/icons-material/StorefrontOutlined'))
-const FastFood = lazy(() => import('@mui/icons-material/FastfoodOutlined'))
-const Cloth = lazy(() => import('@mui/icons-material/CheckroomOutlined'))
-const Electronics = lazy(() => import('@mui/icons-material/BlenderOutlined'))
-const BuildAndRepair = lazy(() => import('@mui/icons-material/ConstructionOutlined'))
-const HomeThings = lazy(() => import('@mui/icons-material/KingBedOutlined'))
-const Beauty = lazy(() => import('@mui/icons-material/SpaOutlined'))
-const PetSupplies = lazy(() => import('@mui/icons-material/PetsOutlined'))
-const Books = lazy(() => import('@mui/icons-material/AutoStoriesOutlined'))
-const Pharmacy = lazy(() => import('@mui/icons-material/VaccinesOutlined'))
-const Health = lazy(() => import('@mui/icons-material/MedicalInformationOutlined'))
-const Fuel = lazy(() => import('@mui/icons-material/LocalGasStationOutlined'))
-const CarService = lazy(() => import('@mui/icons-material/BuildOutlined'))
-const CarParts = lazy(() => import('@mui/icons-material/SettingsOutlined'))
-const Parking = lazy(() => import('@mui/icons-material/LocalParkingOutlined'))
-const Subscription = lazy(() => import('@mui/icons-material/CloudDoneOutlined'))
-const Games = lazy(() => import('@mui/icons-material/SportsEsportsOutlined'))
-const Marketplace = lazy(() => import('@mui/icons-material/ShoppingCartOutlined'))
-const PublicTransport = lazy(() => import('@mui/icons-material/DirectionsBusOutlined'))
-const Taxi = lazy(() => import('@mui/icons-material/LocalTaxiOutlined'))
-const JKH = lazy(() => import('@mui/icons-material/RealEstateAgentOutlined'))
-const Communication = lazy(() => import('@mui/icons-material/RouterOutlined'))
-const Finance = lazy(() => import('@mui/icons-material/SavingsOutlined'))
-const Education = lazy(() => import('@mui/icons-material/SchoolOutlined'))
-const Entertainment = lazy(() => import('@mui/icons-material/LocalActivityOutlined'))
-const Sport = lazy(() => import('@mui/icons-material/FitnessCenterOutlined'))
-const Trip = lazy(() => import('@mui/icons-material/AirplanemodeActiveOutlined'))
-const Charity = lazy(() => import('@mui/icons-material/AutoAwesomeOutlined'))
-const Flowers = lazy(() => import('@mui/icons-material/LocalFlorist'))
-const Other = lazy(() => import('@mui/icons-material/ShoppingBagOutlined'))
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+type IconComponent = OverridableComponent<SvgIconTypeMap<{}, 'svg'>>
 
-export const CATEGORIES_ICONS_MAP = new Map<
-  number,
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  React.LazyExoticComponent<OverridableComponent<SvgIconTypeMap<{}, 'svg'>>>
->([
-  [0, Supermarkets],
-  [1, FastFood],
-  [2, Cloth],
-  [3, Electronics],
-  [4, BuildAndRepair],
-  [5, HomeThings],
-  [6, Beauty],
-  [7, PetSupplies],
-  [8, Books],
-  [9, Pharmacy],
-  [10, Health],
-  [11, Fuel],
-  [12, CarService],
-  [13, CarParts],
-  [14, Parking],
-  [15, Subscription],
-  [16, Games],
-  [17, Marketplace],
-  [18, PublicTransport],
-  [19, Taxi],
-  [20, JKH],
-  [21, Communication],
-  [22, Finance],
-  [23, Education],
-  [24, Entertainment],
-  [25, Sport],
-  [26, Trip],
-  [27, Charity],
-  [28, Flowers],
-  [29, Other],
+export const CATEGORIES_ICONS_MAP = new Map<number, IconComponent>([
+  [0, StorefrontOutlined],
+  [1, FastfoodOutlined],
+  [2, CheckroomOutlined],
+  [3, BlenderOutlined],
+  [4, ConstructionOutlined],
+  [5, KingBedOutlined],
+  [6, SpaOutlined],
+  [7, PetsOutlined],
+  [8, AutoStoriesOutlined],
+  [9, VaccinesOutlined],
+  [10, MedicalInformationOutlined],
+  [11, LocalGasStationOutlined],
+  [12, BuildOutlined],
+  [13, SettingsOutlined],
+  [14, LocalParkingOutlined],
+  [15, CloudDoneOutlined],
+  [16, SportsEsportsOutlined],
+  [17, ShoppingCartOutlined],
+  [18, DirectionsBusOutlined],
+  [19, LocalTaxiOutlined],
+  [20, RealEstateAgentOutlined],
+  [21, RouterOutlined],
+  [22, SavingsOutlined],
+  [23, SchoolOutlined],
+  [24, LocalActivityOutlined],
+  [25, FitnessCenterOutlined],
+  [26, AirplanemodeActiveOutlined],
+  [27, AutoAwesomeOutlined],
+  [28, LocalFlorist],
+  [29, ShoppingBagOutlined],
 ])
 
 export const CATEGORY_IDS = Array.from(CATEGORIES_ICONS_MAP.keys())
