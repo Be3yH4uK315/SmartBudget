9fc02f3e054306f121f56471eb2231b5884e8d05
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-16 13:07:56 +0700

    ref: remove Suspense from CategoryIcon

--- frontend/src/features/transactions/components/CategoryIcon.tsx
+++ frontend/src/features/transactions/components/CategoryIcon.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react-hooks/static-components */
-import { Suspense } from 'react'
 import { CATEGORIES_ICONS_MAP } from '@features/transactions/constants/categoriesIcons'
 import { ShoppingBagOutlined } from '@mui/icons-material'
 import { Box, SxProps } from '@mui/material'
-import { useTheme } from '@shared/hooks'
 
 type Props = {
   categoryId: number
@@ -20,22 +18,20 @@ export const CategoryIcon = ({ categoryId, size = 70, boxSx, iconSx }: Props) =>
   }
 
   return (
-    <Suspense fallback={<Box width={size} height={size} />}>
-      <Box
-        sx={{
-          width: size,
-          height: size,
-          bgcolor: 'surface.light',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          p: 8,
-          borderRadius: `${size}px`,
-          ...boxSx,
-        }}
-      >
-        <IconComponent sx={{ width: size / 1.5, height: size / 1.5, ...iconSx }} />
-      </Box>
-    </Suspense>
+    <Box
+      sx={{
+        width: size,
+        height: size,
+        bgcolor: 'surface.light',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        p: 8,
+        borderRadius: `${size}px`,
+        ...boxSx,
+      }}
+    >
+      <IconComponent sx={{ width: size / 1.5, height: size / 1.5, ...iconSx }} />
+    </Box>
   )
 }
