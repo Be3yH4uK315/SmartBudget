df3f76a766c0df183465169e8e5968b5a9039724
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-16 13:12:49 +0700

    ref: move MUIComponents out of TransationsList

--- frontend/src/features/transactions/screens/TransactionsScreen/TransactionList.tsx
+++ frontend/src/features/transactions/screens/TransactionsScreen/TransactionList.tsx
@@ -1,12 +1,13 @@
-import { forwardRef, useCallback, useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import 'dayjs/locale/ru'
+import { ListFooter, MUIComponents } from '@features/transactions/components'
 import { getTransactions } from '@features/transactions/store'
 import { TransactionsBlock } from '@features/transactions/types'
 import { normalizeBlocksList } from '@features/transactions/utils'
-import { Box, CircularProgress, List, ListItem, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { useAppDispatch } from '@shared/store'
 import dayjs from 'dayjs'
-import { GroupedVirtuoso, GroupedVirtuosoProps } from 'react-virtuoso'
+import { GroupedVirtuoso } from 'react-virtuoso'
 import { TransactionLine } from './TransactionLine'
 
 type Props = {
@@ -20,42 +21,6 @@ export const TransactionsList = ({ isLast, isLoading, transactions }: Props) => 
 
   const normalizedBlocks = useMemo(() => normalizeBlocksList(transactions), [transactions])
 
-  const MUIComponents: GroupedVirtuosoProps<unknown, unknown>['components'] = {
-    List: forwardRef(({ style, children, ...props }, ref) => (
-      <List
-        ref={ref}
-        {...props}
-        component="div"
-        style={{ ...style }}
-        sx={{ padding: 0, margin: 0, maxWidth: '800px' }}
-      >
-        {children}
-      </List>
-    )),
-
-    Item: ({ children, ...props }) => (
-      <ListItem component={'div'} {...props} sx={{ px: 0, py: 1 }}>
-        {children}
-      </ListItem>
-    ),
-
-    Group: ({ children }) => (
-      <Box component={'div'} sx={{ bgcolor: 'surface.main', py: 2 }}>
-        {children}
-      </Box>
-    ),
-  }
-
-  const Footer = () => {
-    if (!isLast && isLoading) {
-      return (
-        <Box sx={{ display: 'flex', maxWidth: '800px', justifyContent: 'center', py: 2 }}>
-          <CircularProgress size={24} />
-        </Box>
-      )
-    }
-  }
-
   const loadMore = useCallback(() => {
     dispatch(getTransactions())
   }, [dispatch])
@@ -63,7 +28,10 @@ export const TransactionsList = ({ isLast, isLoading, transactions }: Props) => 
   return (
     <GroupedVirtuoso
       style={{ height: '100%' }}
-      components={{ ...MUIComponents, Footer }}
+      components={{
+        ...MUIComponents,
+        Footer: () => <ListFooter isLast={isLast} isLoading={isLoading} />,
+      }}
       useWindowScroll
       increaseViewportBy={{ bottom: 400, top: 0 }}
       overscan={200}
