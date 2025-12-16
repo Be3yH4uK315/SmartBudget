d20678e51a3fa51df29753fb2ef4955c49ddb953
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-16 23:44:27 +0700

    feat: add goals locale

---
+++ frontend/src/features/goals/locale/goals.dict.ts
@@ -0,0 +1,30 @@
+import { LocaleDictionary } from '@shared/types'
+
+export const goalsDict: LocaleDictionary = {
+  ru: {
+    Goals: {
+      noGoals: {
+        title: 'Целей нет',
+        subtitle: 'Здесь можно создать цель и копить на приятные мелочи',
+      },
+      title: 'Ваши цели',
+      create: 'Создать цель',
+      edit: 'Сохранить изменения',
+      editGoal: 'Редактирвоание Цели',
+      createGoal: 'Создание цели',
+      date: 'Дата окончания',
+      name: 'Название',
+      value: 'Сумма',
+    },
+  },
+  en: {
+    Goals: {
+      noGoals: {
+        title: '',
+        subtitle: '',
+      },
+      title: '',
+      create: '',
+    },
+  },
+}
