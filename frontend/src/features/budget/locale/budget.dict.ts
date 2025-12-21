cb3d66784a89d2166d191c3590d6bd1f9ff43c4a
anton <sedleczkij.2022@stud.nstu.ru>
2025-12-21 19:07:53 +0700

    feat: add budget dict

---
+++ frontend/src/features/budget/locale/budget.dict.ts
@@ -0,0 +1,96 @@
+import { LocaleDictionary } from '@shared/types'
+
+export const budgetDict: LocaleDictionary = {
+  ru: {
+    Budget: {
+      title: 'Бюджет',
+      settingsButtonTitle: 'Настроить бюджет',
+      settingsButtonSubtitle:
+        'Установка и редактирование лимита на бюджет, управление лимитами категорий',
+
+      transactionsBlockTitle: 'Текущие расходы',
+      income: 'Доходы',
+
+      cannotFindData: 'К сожалению, у нас что-то сломалось :(',
+      tryAgain: 'Попробуйте обновить страницу или загляните позже - мы обязательно все починим',
+
+      CategoryBlock: {
+        category: 'Лимиты не установлены',
+        category_one: '{{count}} категория',
+        category_few: '{{count}} категории',
+        category_many: '{{count}} категорий',
+
+        title: 'Количество категорий с установленным лимитом',
+        subtitle: 'Пришлем пуш-уведомление, когда достигните лимита в 80 и 100 %',
+      },
+
+      FactExpense: {
+        title: 'Фактические расходы',
+        subtitle: 'На основе ваших операций',
+      },
+
+      PlanExpense: {
+        title: 'Запланированные расходы',
+        subtitle: 'На основе лимитов по категориям и лимита бюджета',
+        subtitleByCategories: 'На основе лимитов по категориям',
+      },
+
+      IsAutoRenew: {
+        title: 'Автопродление бюджета',
+        on: 'Включено',
+        off: 'Выключено',
+      },
+
+      Modal: {
+        createTitle: 'Создание бюджета',
+
+        totalLimitTitle: 'Общий лимит',
+        totalLimitSubtitle: 'Сумма, которую планируете потратить',
+        totalLimit: 'Сумма',
+
+        addCategory: 'Добавить категорию',
+        autoRenew: 'Автопродление',
+        create: 'Создать бюджет',
+
+        categoriesTitle: 'Лимиты по категориям',
+        categoriesSubtitle: 'Лимит для каждой категории. Можно изменить в настройках бюджета',
+        selectPlaceholder: 'Категория',
+        limit: 'Лимит категории, руб',
+        percentSumError: 'Сумма процентов должна быть равна 100',
+      },
+    },
+
+    Overflow: {
+      overflowTitle: 'Превышение лимита',
+      overflowSubtitle_one: 'Вы превысили лимит в {{count}} категории:',
+      overflowSubtitle_few: 'Вы превысили лимит в {{count}} категориях:',
+      overflowSubtitle_many: 'Вы превысили лимит в {{count}} категориях:',
+
+      preOverflowTitle: 'Превышение лимита',
+      preOverflowSubtitle_one: 'Вы почти превысили лимит в {{count}} категории:',
+      preOverflowSubtitle_few: 'Вы почти превысили лимит в {{count}} категориях:',
+      preOverflowSubtitle_many: 'Вы почти превысили лимит в {{count}} категориях:',
+    },
+
+    CategoryLimitBlock: {
+      limitedTitle: 'Лимиты по категориям',
+      limitedSubtitle: 'Категории, на которые вы установили лимит',
+      unlimitedTitle: 'Категории без лимитов',
+      unlimitedSubtitle: 'Установите лимит, чтобы держать расходы под контролем',
+    },
+  },
+
+  en: {
+    Budget: {
+      CategoryBlock: {
+        category: '',
+        category_one: '',
+        category_few: '',
+        category_many: '',
+
+        title: '',
+        subtitle: '',
+      },
+    },
+  },
+}
