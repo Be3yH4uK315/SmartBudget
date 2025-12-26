ed2358e1ef26b6268b3468bce1ee7777ff62f7e2
Be3yH4uK315 <dkosterin3@gmail.com>
2025-12-26 18:47:46 +0700

    feat: Added uow, rules

--- services/classification/app/dependencies.py
+++ services/classification/app/dependencies.py
@@ -1,31 +1,27 @@
 from fastapi import Depends, Request, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
 from redis.asyncio import Redis, ConnectionPool
-from arq.connections import ArqRedis
-from aiokafka import AIOKafkaProducer
 from typing import AsyncGenerator
 
-from app import settings
+from app import settings, unit_of_work
 from app.services.classification_service import ClassificationService
 from app.services.ml_service import modelManager
+from app.services.rule_manager import ruleManager
 
-async def get_db(request: Request) -> AsyncGenerator[AsyncSession, None]:
-    """Обеспечивает асинхронный сеанс работы с БД."""
+async def get_uow(request: Request) -> AsyncGenerator[unit_of_work.UnitOfWork, None]:
+    """Создает UnitOfWork с фабрикой сессий из app.state."""
     db_session_maker = request.app.state.db_session_maker
     if not db_session_maker:
-        raise HTTPException(status_code=503, detail="Database session factory not available")
-
-    async with db_session_maker() as session:
-        yield session
+        raise HTTPException(status_code=500, detail="Database session factory not available")
+    yield unit_of_work.UnitOfWork(db_session_maker)
 
 async def get_redis(request: Request) -> AsyncGenerator[Redis, None]:
     """Обеспечивает подключение Redis из пула."""
     pool: ConnectionPool = request.app.state.redis_pool
-    redis = Redis(connection_pool=pool, decode_responses=True)
+    client = Redis(connection_pool=pool, decode_responses=True)
     try:
-        yield redis
+        yield client
     finally:
-        await redis.aclose()
+        await client.aclose()
 
 async def create_redis_pool() -> ConnectionPool:
     """Создает пул подключений Redis."""
@@ -35,29 +31,19 @@ async def close_redis_pool(pool: ConnectionPool) -> None:
     """Закрывает пул подключений Redis."""
     await pool.disconnect()
 
-async def get_arq_pool(request: Request) -> AsyncGenerator[ArqRedis, None]:
-    """Предоставляет пул Arq."""
-    arq_pool: ArqRedis = request.app.state.arq_pool
-    try:
-        yield arq_pool
-    finally:
-        pass
-
-async def get_kafka_producer(request: Request) -> AIOKafkaProducer:
-    """Получает Kafka-продюсер."""
-    kafka_producer: AIOKafkaProducer = request.app.state.kafka_producer
-    if not kafka_producer:
-        raise HTTPException(status_code=503, detail="Kafka producer not initialized")
-    return kafka_producer
-
-def get_ml_pipeline(request: Request) -> dict | None:
+def get_ml_pipeline() -> dict | None:
     """Получает актуальный ML-пайплайн из менеджера."""
     return modelManager.get_pipeline()
 
+def get_active_rules() -> list[dict]:
+    """Зависимость для получения правил из памяти."""
+    return ruleManager.get_rules()
+
 async def get_classification_service(
-    db: AsyncSession = Depends(get_db),
+    uow: unit_of_work.UnitOfWork = Depends(get_uow),
     redis: Redis = Depends(get_redis),
-    ml_pipeline: dict | None = Depends(get_ml_pipeline)
+    pipeline: dict = Depends(get_ml_pipeline),
+    rules: list[dict] = Depends(get_active_rules)
 ) -> ClassificationService:
     """Фабрика зависимостей для ClassificationService."""
-    return ClassificationService(db, redis, ml_pipeline)
+    return ClassificationService(uow, redis, pipeline, rules)
