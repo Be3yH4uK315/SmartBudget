76c5b2ff655a86707ed38a3dba6c4a8721d14f0d
Be3yH4uK315 <dkosterin3@gmail.com>
2026-01-08 21:52:43 +0700

    feat: Added pre ping for DB

--- services/goals/app/core/database.py
+++ services/goals/app/core/database.py
@@ -7,7 +7,8 @@ def get_db_engine():
         settings.DB.DB_URL,
         pool_size=settings.DB.DB_POOL_SIZE,
         max_overflow=settings.DB.DB_MAX_OVERFLOW,
-        echo=False
+        echo=False,
+        pool_pre_ping=True 
     )
 
 def get_session_factory(engine):
