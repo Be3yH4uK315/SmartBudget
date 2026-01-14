4b02dd086227a691c58bfc6b12ea1225f03f082c
Be3yH4uK315 <dkosterin3@gmail.com>
2026-01-14 22:06:05 +0700

    feat: Added new key

--- services/authentification/app/utils/redis_keys.py
+++ services/authentification/app/utils/redis_keys.py
@@ -1,15 +1,19 @@
 def get_verify_email_key(email: str) -> str:
     """Ключ Redis для токена верификации email."""
-    return f"verify:{email}"
+    return f"auth:verify:{email}"
 
 def get_reset_password_key(email: str) -> str:
     """Ключ Redis для токена сброса пароля."""
-    return f"reset:{email}"
+    return f"auth:reset:{email}"
 
 def get_login_fail_key(ip: str) -> str:
     """Ключ Redis для отслеживания неудачных попыток входа по IP."""
-    return f"fail:{ip}"
+    return f"auth:fail:{ip}"
 
 def get_session_key(session_id: str) -> str:
     """Ключ для кэширования активной сессии."""
-    return f"session:active:{session_id}"
+    return f"auth:session:active:{session_id}"
+
+def get_user_cache_key(user_id: str) -> str:
+    """Ключ для кэширования объекта User."""
+    return f"auth:user:data:{user_id}"
