54e575ba5e28e3d8b189ed09d84ff3abfa6103db
Be3yH4uK315 <dkosterin3@gmail.com>
2026-01-17 19:43:56 +0700

    refactor: Using enums

--- services/goals/app/domain/schemas/kafka.py
+++ services/goals/app/domain/schemas/kafka.py
@@ -1,7 +1,8 @@
-from enum import Enum
-from pydantic import BaseModel, Field, ConfigDict
-from decimal import Decimal
 from uuid import UUID
+from decimal import Decimal
+from pydantic import BaseModel, Field, ConfigDict
+
+from app.domain.enums import TransactionType
 
 def to_camel(string: str) -> str:
     parts = string.split("_")
@@ -11,21 +12,8 @@ class CamelModel(BaseModel):
     model_config = ConfigDict(
         alias_generator=to_camel,
         populate_by_name=True,
-        json_encoders={Decimal: float}
+        json_encoders={Decimal: float},
     )
-
-class GoalEventType(str, Enum):
-    CREATED = "goal.created"
-    CHANGED = "goal.changed"
-    UPDATED = "goal.updated"
-    ACHIEVED = "goal.achieved"
-    EXPIRED = "goal.expired"
-    APPROACHING = "goal.approaching"
-    ALERT = "goal.alert"
-
-class TransactionType(str, Enum):
-    INCOME = "income"
-    EXPENSE = "expense"
 
 class TransactionEvent(CamelModel):
     transaction_id: UUID = Field(..., description="ID транзакции")
@@ -34,4 +22,6 @@ class TransactionEvent(CamelModel):
     value: Decimal = Field(..., gt=0, description="Сумма транзакции")
     type: TransactionType = Field(..., description="Тип транзакции")
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
