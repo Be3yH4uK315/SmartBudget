8577655048163d22db26a85b3425fa8f3b0ee978
anton <sedleczkij.2022@stud.nstu.ru>
2026-01-21 07:28:49 +0700

    style: update styles

--- frontend/src/shared/styles/lightTheme.ts
+++ frontend/src/shared/styles/lightTheme.ts
@@ -1,17 +1,21 @@
 import { createTheme } from '@mui/material'
+import { ruRU } from '@mui/x-date-pickers/locales'
 import { breakpoints } from './breakpoints'
 import { components } from './components'
 import { paletteLight } from './paletteLight'
 import { shadows } from './shadows'
 import { typography } from './typography'
 
-export const lightTheme = createTheme({
-  typography,
-  palette: paletteLight,
-  components,
-  shadows,
-  breakpoints,
-  shape: {
-    borderRadius: 6,
+export const lightTheme = createTheme(
+  {
+    typography,
+    palette: paletteLight,
+    components,
+    shadows,
+    breakpoints,
+    shape: {
+      borderRadius: 6,
+    },
   },
-})
+  ruRU,
+)
