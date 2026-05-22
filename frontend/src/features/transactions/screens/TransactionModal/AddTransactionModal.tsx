f531291fee650bbbd59539f309be5cdbe95c23a5
Be3yH4uK315 <dkosterin3@gmail.com>
2026-05-22 17:14:42 +0700

    chore: cur front

--- frontend/src/features/transactions/screens/TransactionModal/AddTransactionModal.tsx
+++ frontend/src/features/transactions/screens/TransactionModal/AddTransactionModal.tsx
@@ -196,7 +196,6 @@ export const AddTransactionModal = ({ onClose, onSuccess }: Props) => {
                     },
                   },
                 }}
-                disablePast
               />
             </LocalizationProvider>
 
